fb9d9cd57bdbe510db62795464845d64b4b5a123
Vis Vasanth <viscodesdpt@gmail.com>
2016-11-18 13:11:58 -0600

    Should we use Sequelize______ or DataTypes___

--- models/recipes.js
+++ models/recipes.js
@@ -1,60 +1,60 @@
-"use strict"; 
+"use strict";
 
-module.exports = function(sequelize, DataTypes){
-var Recipes = sequelize.define('Recipes', { 
-        RecipeID: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true, // not null built into this 
-        },
-        RecipeName: {
-          type: DataTypes.STRING(50), 
-          unique: true // allows only unique string in field when the update stuff for sql runs 
-        },
-        IngredID: {
-          type: DataTypes.STRING(50), 
-          references: {
-              model: 'Ingredients',
-              key: 'IngredID'
+module.exports = function(sequelize, DataTypes) {
+    var Recipes = sequelize.define('Recipes', {
+            RecipeID: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true, // not null built into this 
             },
-          onUpdate: 'cascade',
-          onDelete: 'cascade'            
-        },
-        StepsID: {
-          type: DataTypes.STRING(50), 
-          references: {
-              model: 'Steps',
-              key: 'StepsID'
+            RecipeName: {
+                type: DataTypes.STRING(50),
+                unique: true // allows only unique string in field when the update stuff for sql runs 
             },
-          onUpdate: 'cascade',
-          onDelete: 'cascade'            
-        },
-        CategoryID: {
-          type: DataTypes.INTEGER,
-          allowNull: false, 
-          references: {
-            model: 'RecipeCategory',
-            key: 'CategoryID'
-          },
-          onUpdate: 'cascade',
-          onDelete: 'cascade'         
-        }, 
-        AllergyID: {
-          type: DataTypes.INTEGER, 
-          allowNull: false,
-          references: {
-            model: 'Allergy',
-            key: 'AllergyID'
-          },
-          onUpdate: 'cascade',
-          onDelete: 'cascade'         
-        }, 
-        classMethods: {
-             associate: function(models) {
-              Recipe.belongsToMany(models.Allergy, {through: 'RecipeAllergy'})
-              }
+            IngredID: {
+                type: DataTypes.STRING(50),
+                references: {
+                    model: 'Ingredients',
+                    key: 'IngredID'
+                },
+                onUpdate: 'cascade',
+                onDelete: 'cascade'
+            },
+            StepsID: {
+                type: DataTypes.STRING(50),
+                references: {
+                    model: 'Steps',
+                    key: 'StepsID'
+                },
+                onUpdate: 'cascade',
+                onDelete: 'cascade'
+            },
+            CategoryID: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'RecipeCategory',
+                    key: 'CategoryID'
+                },
+                onUpdate: 'cascade',
+                onDelete: 'cascade'
+            },
+            AllergyID: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                references: {
+                    model: 'Allergy',
+                    key: 'AllergyID'
+                },
+                onUpdate: 'cascade',
+                onDelete: 'cascade'
+            },
+            classMethods: {
+                associate: function(models) {
+                    Recipe.belongsToMany(models.Allergy, { through: 'RecipeAllergy' })
+                }
             }
         }
-      });
-      return Recipes;
+    });
+return Recipes;
 }
--- models/users.js
+++ models/users.js
@@ -1,41 +1,41 @@
-"use strict"; 
+"use strict";
 
-module.exports = function(sequelize, DataTypes){
-var Users = sequelize.define('User', { 
+module.exports = function(sequelize, DataTypes) {
+    var Users = sequelize.define('User', {
         UserID: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          primaryKey: true
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
         },
         FirstName: {
-          type: Sequelize.STRING(20), 
-          allowNull: false 
+            type: DataTypes.STRING(20),
+            allowNull: false
         },
         LastName: {
-          type: Sequelize.STRING(25),
-          allowNull: false
-        }, 
+            type: DataTypes.STRING(25),
+            allowNull: false
+        },
         email: {
-          type: Sequelize.STRING(50),
-          allowNull: false
-        }, 
+            type: DataTypes.STRING(50),
+            allowNull: false
+        },
         Password: {
-          type: Sequelize.STRING(20),
-          allowNull: false,
-          // this is for validation. 
-          	// len is a property that will make sure the characters added will be between 3 and 15 characters, otherwise it will error  
-          validate: {
-          	len: {
-          		args: [3, 15],  // argument for len is to make it between 3 and 15 characters 
-          		msg: "Please enter a password between 3 and 15 characters" // will display this error if args are not fulfilled
-          		}
-          	}
-          },
+            type: DataTypes.STRING(20),
+            allowNull: false,
+            // this is for validation. 
+            // len is a property that will make sure the characters added will be between 3 and 15 characters, otherwise it will error  
+            validate: {
+                len: {
+                    args: [3, 15], // argument for len is to make it between 3 and 15 characters 
+                    msg: "Please enter a password between 3 and 15 characters" // will display this error if args are not fulfilled
+                }
+            }
+        },
         // timestamps: false,
         // ^^^ NEED THIS ^^^ without this, sequelize will create additional columns called 'createAt' and 'updateAt'
-        	// 'createAt': timestamp of record created 
-        	// 'updateAt': timestamp of record updated 
+        // 'createAt': timestamp of record created 
+        // 'updateAt': timestamp of record updated 
         // freezeTableName: true // sequelize automatically puralizes table names, this will prevent that from happening
-      });
-return Users;
-} 
+    });
+    return Users;
+}
