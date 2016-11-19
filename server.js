1ccf954820ffe096b9769e6d3c64f2451ffb7465
Vis Vasanth <viscodesdpt@gmail.com>
2016-11-19 09:05:25 -0600

    AHHHHH server works

--- server.js
+++ server.js
@@ -4,8 +4,8 @@
 
 // Dependencies
 // =============================================================
-var express 	= require('express');
-var bodyParser 	= require('body-parser');
+var express = require('express');
+var bodyParser = require('body-parser');
 
 
 
@@ -17,9 +17,9 @@ var PORT = process.env.PORT || 8080; // this is for heroku setup. Will look for 
 
 // Sets up the Express app to handle data parsing 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // static directory
 app.use(express.static('app/public'));
@@ -28,14 +28,13 @@ app.use(express.static('app/public'));
 // Routes
 // =============================================================
 
-require("./app/routes/api-routes.js")(app)
-require("./app/routes/html-routes.js")(app)
+
 
 
 
 
 // Starts the server to begin listening 
 // =============================================================
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
 })
